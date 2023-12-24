ed6709d16487d35233867407d26b404be1f193d2
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-24 20:32:58 +0000

    Modifications ahdering to requirements

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 class Square:
+
+	"""Defines a square """
+
+
 	def __init__(self, size):
+		"""Initializes a new square.
+
+		Args:
+			size (int): The size of thr square,
+
+		"""
 		self.__size = size
