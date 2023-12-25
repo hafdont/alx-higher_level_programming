38882d3f92b6aaa25ccb7a9f3ea2679a3fb97bda
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-25 16:16:57 +0000

    Proper structure of the code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
+
+	"""a module that defines a square"""
+
 class Square:
-	"""
-	Square class with  private instance attribute size.
-	instantiation with optional size, and public instance method ares.
-	"""
+	""" A class module that represents a square """
 
 
 	def __init__(self, size=0):
 		"""
 		Intitializes a new instance
 
-		Args
+		Args:
 			size (int): THe size of the square
-
 
 		Raises:
 			TypeError: If the size is not an integer.
 			Value Error: If the size is less than ).
 		"""
 
+
 		if not isinstance(size, int):
 			raise TypeError("size must be an integer")
 		elif size < 0:
 			raise ValueError("size must be >= 0")
 		else:
+			
 			self.__size = size
 
 
@@ -31,7 +32,7 @@ class Square:
 		"""
 		Calculates the are of the squre.
 
-		Returns:
-			int: The area of te square
+		Returns: The area of te square
 		"""
+
 		return self.__size ** 2
