0c71d0dacca5f80938e9f12d554ea7d14076f152
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-29 18:16:11 +0000

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    #check if the ASCII code of the character is in the lowercase range
+    # check if the ASCII code of the character is in the lowercase range
     return ord('a') <= ord(c) <= ord('z')
