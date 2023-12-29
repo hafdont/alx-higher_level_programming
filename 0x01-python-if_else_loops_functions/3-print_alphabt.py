33e173cfa61398460cbdb3296091d400c2c624cf
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-29 12:26:29 +0000

    Doesnt time fly be when you sre on your keyboard

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 for c in range(ord('a'), ord('z') + 1):
-	if chr(c) not in 'qe':
-		print("{}".format(chr(c)), end="")
+    if chr(c) not in 'qe':
+        print("{}".format(chr(c)), end="")
+
