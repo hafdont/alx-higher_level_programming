a5bd4c73e0320fd40b417f9bc77f002792af161c
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-30 01:40:04 +0000

    Task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the result.
+
+    Parameters:
+    - a: The base
+    - b: The exponent
+
+    Returns:
+    - The result of a ^ b 
+    """
+    # Check if the exxponent and base are numbers
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        print("invalid input.")
+        return None
+
+    result = a ** b
+
+    return result
