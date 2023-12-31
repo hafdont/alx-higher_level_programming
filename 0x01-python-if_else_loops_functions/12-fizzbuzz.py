8acdab1fdb8744e6cfa292463133ee00a646dbd6
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-31 09:54:23 +0000

    take 2

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,11 +6,11 @@ def fizzbuzz():
     multipple of 5 with Buzz. and multiples of boh (3 and 5) with FizzBuzz.
     """
     for num in range(1, 101):
-        if num % 3 == 0 and num % 5 ==0:
+        if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz", end=" ")
         elif num % 3 == 0:
             print("Fizz", end=" ")
-        elif num % 5 ==0:
+        elif num % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(num, end=" ")
