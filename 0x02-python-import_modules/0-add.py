cd3b84d161ff47f16bbecafc268121c2e0d1dec9
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-12-31 06:46:14 -0800

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
+# Import the add function from the add_0 module
+from add_0 import add
+
+# Assign the value 1 to variable a
 a = 1
+
+# Assign the value 2 to variable b
 b = 2
 
-from add_o import add
 print("{} + {} = {}".format(a, b, add(a, b)))
