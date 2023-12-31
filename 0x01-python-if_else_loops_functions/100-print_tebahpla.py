192a2c7ee855b8250c0b3b4d2db2bc0bf9f72ca1
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-31 13:01:45 +0000

    Pledge allegence

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# Print the ASCII alphabet in revese order, alternating between upper and lower cases
+# Print the ASCII alphabet in revese order,
 
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(i), end="")
+    print(chr(i), end="")
     i -= 32 if i % 2 == 0 else 0
