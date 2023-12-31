bd25004bae9eea56fc1e32f3bf00abd6ffee71a2
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-31 14:08:11 +0000

    We put it on like big L

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b, c):
-    # Check if 'a' is less than 'b'
+def magic_calculation(a,b,c)
+
+    # Check if a 'a' is less than 'b'
     if a < b:
-        # If true, return the value of 'c'
+        # If true, return the value of C
         return c
-    # Check if 'c' is greater than 'b'
     elif c > b:
-        # If true, return the sum of 'a' and 'b'
-        return a + b
+        # if true, return the sum of 'a' and 'b'
+        reurn a + b
     else:
-        # If none of the above conditions are met, return the result of 'a * b - c'
-        return a * b - c
+        return a * b - c 
