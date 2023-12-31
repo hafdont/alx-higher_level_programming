5628bf90044bb27070552dffbe09905419a88991
Hafiz Yusuf <hafizmasoud&@gmail.com>
2023-12-31 13:23:05 +0000

    Fixing errors

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,10 +2,10 @@
 
 # Print the ASCII alphabet in revese order,
 
+# Print the ASCII alphabet in reverse order, alternating upper and lower cases
+
 output = ""
-
 for i in range(ord('z'), ord('a') - 1, -1):
-    output += chr(i)
-    i -= 32 if i % 2 == 0 else 0
+    output += "{:c}".format(i) if i % 2 == 0 else "{:c}".format(i -32)
 
-print(output)
+print(output, end="")
