f48f289bb5ac37f6495c0eda85c0da587c07369b
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-01-01 00:29:29 -0800

    Happy new year

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    # Import the add function from the add_0 module
     from add_0 import add
-
-    # Assign the value 1 to variable a
     a = 1
-
-    # Assign the value 2 to variable b
     b = 2
-
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
