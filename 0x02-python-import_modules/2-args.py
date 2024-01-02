bc379665649d983d1369749f5941aeb16b9d2844
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-01-02 01:28:14 -0800

    Task 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-    from sys import argv
-    # Get the number of argumenets
-    num_args = len(argv) - 1
+    import sys
 
-    # Print the number of arguments
-    print("{} arguments{}{}".format(
-        num_args,
-        's' if num_args != 1 else '',
-        '' if num_args == 0 else ':'
-    ))
+    # Get the number of arguments
+    num_args = len(sys.argv) - 1
 
-    # Print each argument and its positiion
-    for i, arg in enumerate(argv[1:], start=1):
+    # Check and print based on the number of arguments
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    # Print each argument and its position
+    for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
+
