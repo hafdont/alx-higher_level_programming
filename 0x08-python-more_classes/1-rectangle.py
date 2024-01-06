e26ce98a170975b023c9497987ad76ba9e5cf5d3
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-01-05 17:30:13 -0800

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,7 +7,6 @@ Defines a class Rectangle
 class Rectangle:
     """Representation of a rectangle"""
 
-
     def __init__(self, width=0, height=0):
         """Initializes the rectangle instance"""
         self.width = width
