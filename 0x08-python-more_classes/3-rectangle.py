6e8ab891658d5b5f868f613279336c8f9ba4b9b0
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-01-06 06:48:26 -0800

    For task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,4 +58,4 @@ class Rectangle:
 
     def __repr__(self):
         """Returns a string representation of the rectangle"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "<{} object at {}>".format(type(self).__name__, hex(id(self)))
