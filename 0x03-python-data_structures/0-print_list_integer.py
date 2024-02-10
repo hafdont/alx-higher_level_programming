925319a5cf249f88ee04558dcb51954a65ccb448
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-02-09 23:09:52 -0800

    Task 1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+def print_list_integer(my_list=[]):
+    """
+    Print all integers of a list, one integer per line.
+
+    Args: 
+        my_list (list): The list of integers to be printed.
+
+    Returns:
+        None
+    """
+
+    for num in my_list:
+        # Print each integer using str.format() without any additional formatt
+        print("{:d}".format(num))
