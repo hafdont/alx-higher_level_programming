466718bcf4c2a778ec8e4851954c2a668d22ff7f
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-02-18 07:08:43 -0800

    Task 0

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-
-# MySQL credentials
-MYSQL_USER="your_username"
-MYSQL_PASSWORD="your_password"
-
-# Use mysql command to connect to MySQL server and execute SHOW DATABASES;
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;"
+-- list all databases of the MySQL server
+SHOW DATABASES;
