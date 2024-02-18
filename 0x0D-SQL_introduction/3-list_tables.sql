25046bb584c70462fac95893ee167ab59007dc01
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-02-18 07:32:11 -0800

    Plus corrections

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-# Check if the database name is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-# Store the database name from the argument
-db_name="$1"
-
-# Use mysql command to list all tables in the specified database
-mysql -uroot -proot -e "USE $db_name; SHOW TABLES;"
+-- list all the tables of database passed
+-- as argument of mysql command
+SHOW TABLES;
