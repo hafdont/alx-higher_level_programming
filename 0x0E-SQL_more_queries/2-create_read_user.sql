e1841aa96bf80dcd33bacfd311b233d01fc2985d
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-03 09:55:47 +0300

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
 -- Create database hbtn_0d_2 if not exists
 -- Grant SELECT privilege on database hbtn_0d_2 to user_0d_2
 -- Create MySQL server user user_0d_2 if not exists
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
