7346d04049b6a3d6c173509ebec2298efdd703ce
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-03 12:53:03 +0300

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,14 +1,10 @@
--- Creates a database hbtn_0d_usa witht the table cities.
--- Grant SELECT privilege on database hbtn_0d_2 to user_0d_2
--- Create MySQL server user user_0d_2 if not exists
-
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS TABLE 'hbtn_0d_usa'.'cities' (
-	PRIMARY KEY('id'),
-	'id' INT AUTO_INCREMENT PRIMARY KEY,
-	'state_id' INT NOT NULL,
-	'name' VARCHAR(256) NOT NULL,
-	FOREIN KEY ('state_id')
-	REFERENCES 'hbtn_0d_usa'.'states'('id')
+-- Creates the database hbtn_0d_usa with the table cities.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    PRIMARY KEY(`id`),
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `state_id` INT          NOT NULL,
+    `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
