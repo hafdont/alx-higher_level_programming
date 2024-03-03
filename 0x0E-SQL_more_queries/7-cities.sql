f08d576551e086e329ad0921f7e78d2d34484fef
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-03 10:55:29 +0300

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,8 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS TABLE cities (
-'id' INT AUTO_INCREMENT PRIMARY KEY,
-'state_id' INT NOT NULL,
-'name' VARCHAR(256) NOT NULL,
-FOREIN KEY (state_id) REFERENCES states(id)
+	PRIMARY KEY('id'),
+	'id' INT AUTO_INCREMENT PRIMARY KEY,
+	'state_id' INT NOT NULL,
+	'name' VARCHAR(256) NOT NULL,
+	FOREIN KEY ('state_id')
+	REFERENCES 'hbtn_0d_usa'.'states'('id')
 );
