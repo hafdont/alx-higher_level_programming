12f1a2f1ed7960af3404bb1561cd7f7a80251941
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-04 10:35:30 +0300

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Saves a Python object to JSON file
+
+    Args:
+        my_obj: The Python object to be serialized.
+        filename: THe name of the file to save the JSON representation
+
+    Returns: 
+        none
+    """
+    try:
+        with open(filename, 'w') as file:
+            json.dump(my_obj, file)
+    except TypeError:
+        pass        
