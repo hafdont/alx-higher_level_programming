24098841b59ca04e5640b7e1bb4cbe9122d2ce04
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-04 11:13:37 +0300

    OH u went

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,16 +3,7 @@
 import json
 
 def save_to_json_file(my_obj, filename):
-    """
-    Saves a Python object to JSON file
-
-    Args:
-        my_obj: The Python object to be serialized.
-        filename: THe name of the file to save the JSON representation
-
-    Returns: 
-        none
-    """
+    """Saves a Python object to JSON file"""
     try:
         with open(filename, 'w') as file:
             json.dump(my_obj, file)
