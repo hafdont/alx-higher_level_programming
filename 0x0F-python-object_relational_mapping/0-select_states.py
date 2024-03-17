2523c3bde8640f9ebe6ebd7d02c169f79988419e
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-17 16:40:27 +0300

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the tabase  `hbtn_0e_0_usa`
+"""
+
+
 import MySQLdb
-import sys
+import sys import argv
 
 if __name__ == "__main__":
+    """
+    Get the args, assign them to usr, pwd, and db,
+    create a cursor, execute a query.
+    show the resutls. then close the cursor and db.
+    """
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database)
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     states = cursor.fetchall()
-    for states in states:
+    for state in states:
         print(states)
 
     cursor.close()
