a6e450e273914b225b13d9d63b3e205d56b8733d
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-19 20:09:25 +0300

    last try

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,15 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the database `hbtn_0e_0_usa`.
-"""
-
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
-
-def list_states(username, password, database):
-    """
-    Connects to the MySQL database and lists all states.
-
-    Args:
-        username (str): The MySQL username.
-        password (str): The MySQL password.
-        database (str): The name of the database.
-
-    Returns:
-        None
-    """
-    db = MySQLdb.connect(host="localhost", user=username, port=3306,
-                         passwd=password, db=database)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
-    states = cursor.fetchall()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
     for state in states:
         print(state)
     cursor.close()
     db.close()
-
-
-if __name__ == '__main__':
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-    else:
-        username, password, database = argv[1:]
-        list_states(username, password, database)
