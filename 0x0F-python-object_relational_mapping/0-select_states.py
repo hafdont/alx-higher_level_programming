1d50b0bf1203ed86f981e450becf0dfe4290be30
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-19 19:37:52 +0300

    Lemon squeeze

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This script lists all states from the database `hbtn_0e_0_usa`"""
 import MySQLdb
-from sys import argv
+import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
