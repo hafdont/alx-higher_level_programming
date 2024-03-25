ef297c091a8c12466e5c290ec6e055df3bb71ced
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-25 11:51:39 +0300

    Task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 
 class Rectangle {
 	constructor (w, h) {
-		if (w > 0 && h > 0) {
+		if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+			return {};
+		} else {
 			this.width = w;
 			this.height = h;
-		} else {
-			return {};
 		}
 	}
 }
