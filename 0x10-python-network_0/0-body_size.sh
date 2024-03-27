583b26f5e9b416e71132d53de34cda6696cce938
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-27 22:42:21 +0300

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  This script takes in a URL, sends a request to that URL using cURL
-curl -sw '%{size_download}\n' -o /dev/null "$1"
+# This script takes in a URL, sends a request to that URL using cURL
+curl -sL -w "%{http_code}" "$1" -o /dev/null | grep -q "200" && curl -s "$1"
