0681596db1d102438e2bc9d3d6790fc9f5a4262e
Hafiz yusuf <hafizmasoud7@gmail.com>
2024-05-12 15:33:59 +0300

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+if (!Number.isNaN(num1) && !Number.isNaN(num2)) {
+  console.log(add(num1, num2));
+} else {
+  console.log('Missing integer(s)');
+}
